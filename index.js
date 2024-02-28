577b9c9dc6900b05062cf2fd847a5767d565f4d5
Mahmoudy96 <mahmoudyaz96@gmail.com>
2024-02-28 11:59:00 +0200

    added info

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+
+let phonebook = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+
+app.get('/api/phonebook', (request,response) => {
+    response.json(phonebook)
+})
+
+app.get('/api/phonebook/:id', (request,response) => {
+    const id = Number(request.params.id)
+    response.json(phonebook.find(person => person.id === id))
+
+})
+
+app.get('/info', (request,response) =>{
+    response.send(
+        `Phonebook contains ${phonebook.length} people        <br/>
+        ${new Date().toLocaleString()}`
+
+        )
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
