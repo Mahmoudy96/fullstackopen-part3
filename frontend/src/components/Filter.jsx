dccec9ba4b431c260e59c9229decabb4d2b7ab2a
Mahmoudy96 <mahmoudyaz96@gmail.com>
2024-12-25 23:16:27 +0200

    removed frontend

--- frontend/src/components/Filter.jsx
+++
@@ -1,8 +0,0 @@
-const Filter = ({value, onChange}) => {
-    return(
-    <div>
-    Filter phonebook by name: <input value={value} onChange={onChange}/>
-    </div>)
-  }
-
-  export default Filter
