3c557d0167c717c567a90a36ffb08ef78bfa40ec
Mahmoudy96 <mahmoudyaz96@gmail.com>
2025-01-22 20:15:13 +0200

    adding put and delete

--- index.js
+++ index.js
@@ -62,10 +62,16 @@ app.get('/api/persons', (request, response) => {
 
 app.get('/api/persons/:id', (request, response) => {
   Person.findById(request.params.id).then(person => {
+    if(person){
     response.json(person)
-  })
+  } else {
+    response.status(404).end()
+  }}
+
+)
     .catch(error => {
-      response.status(404).end()
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' }) 
     }
     )
 })
@@ -79,19 +85,33 @@ app.get('/info', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  phonebook = phonebook.filter(person => person.id !== id)
-  //console.log('id', id)
-
-  response.status(204).end()
-
-})
+  Person.findByIdAndDelete(request.params.id).then(() => {
+    response.status(204).end()
+  })
+}
+)
 const generateId = () => {
   const randId = phonebook.length > 0
     ? Math.floor(Math.random() * 100000)
     : 0
   return randId
 }
+
+
+app.put('/api/persons/:id', (request, response,next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate  (request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
@@ -114,8 +134,17 @@ app.post('/api/persons', (request, response) => {
     response.json(savedPerson)
   })
 })
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
 
+  next(error)
+}
+
+app.use(errorHandler)
 const PORT = process.env.PORT
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
